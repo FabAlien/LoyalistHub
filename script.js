4109e0d95eb38c2e63f9ab2f31a10beeb6d16784
FabAlien <fabalien.von@gmail.com>
2025-11-14 11:40:28 +0100

    First commit

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function burgerFunction() {
     x.className = 'navbar';
   }
 }
+
+
+const container = document.getElementById("extendContainer");
+const card = document.getElementById("cardExtend");
+const openDiv = document.querySelector(".cardContainer");
+
+window.extendInfo = function() {
+  container.classList.add("active");
+  console.log("Success");
+};
+
+window.hideInfo = function() {
+  container.classList.remove("active");
+  console.log("Success");
+};
+
+document.body.addEventListener("click", (event) => {
+  if (
+    container.classList.contains("active") &&
+    !card.contains(event.target) &&
+    event.target !== openDiv
+  ) {
+    hideInfo();
+  }
+});
